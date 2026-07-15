4bc30c9b0c77eb0d48e9d388d078131db52de6d9
Oleg Budaev <t0r1n88@mail.ru>
2026-07-15 08:55:19 +0800

    сделал базовые своды

--- unique_abitur_resp.py
+++ unique_abitur_resp.py
@@ -60,6 +60,7 @@ non_dupl_snils = snils_non_dupl_df.shape[0]
 print(snils_non_dupl_df.shape)
 dupl_df = snils_df[snils_df['СНИЛС абитуриента'].duplicated(keep=False)]
 dupl_df = dupl_df.sort_values(by='СНИЛС абитуриента')
+dupl_snils = dupl_df.shape[0]
 
 freq_stats = dupl_df['СНИЛС абитуриента'].value_counts().value_counts().sort_index()
 df_freq_stats = pd.DataFrame({
@@ -69,7 +70,36 @@ df_freq_stats = pd.DataFrame({
 df_freq_stats = df_freq_stats.sort_values(by='Количество поданных заявлений',ascending=False)
 
 print(df_freq_stats)
-dupl_df.to_excel('data/dupl.xlsx')
+
+# Общий свод по основным показателям
+svod_df = pd.DataFrame({'Показатель':['Общее количество заявлений','Корректные СНИЛС','Некорректные СНИЛС','Уникальные СНИЛС','Повторяющиеся СНИЛС',],
+                        'Значение':[all_value,correct_snils,non_correct_snils,non_dupl_snils,dupl_snils]})
+
+# Общее количество
+poo_svod_all_df = pd.pivot_table(df,index=['ПОО'],
+                                 values=['СНИЛС абитуриента'],
+                                 aggfunc='count',
+                                 margins=True,margins_name='Итого').rename(columns={'СНИЛС абитуриента':'Количество'})
+
+poo_svod_correct_snils_df = pd.pivot_table(snils_df,index=['ПОО'],
+                                 values=['СНИЛС абитуриента'],
+                                 aggfunc='count',
+                                 margins=True,margins_name='Итого').rename(columns={'СНИЛС абитуриента':'Количество'})
+
+poo_svod_non_correct_snils_df = pd.pivot_table(bad_snils_df,index=['ПОО'],
+                                 values=['СНИЛС абитуриента'],
+                                 aggfunc='count',
+                                 margins=True,margins_name='Итого').rename(columns={'СНИЛС абитуриента':'Количество'})
+
+poo_svod_uniq_snils_df = pd.pivot_table(snils_non_dupl_df,index=['ПОО'],
+                                 values=['СНИЛС абитуриента'],
+                                 aggfunc='count',
+                                 margins=True,margins_name='Итого').rename(columns={'СНИЛС абитуриента':'Количество'})
+
+poo_svod_dupl_snils_df = pd.pivot_table(dupl_df,index=['ПОО'],
+                                 values=['СНИЛС абитуриента'],
+                                 aggfunc='count',
+                                 margins=True,margins_name='Итого').rename(columns={'СНИЛС абитуриента':'Количество'})
 
 
 
@@ -77,7 +107,17 @@ dupl_df.to_excel('data/dupl.xlsx')
 
 
 
-df.to_excel('data/test.xlsx',index=False)
+with pd.ExcelWriter(f'data/Свод по абитуриентам {current_time}.xlsx') as writer:
+    svod_df.to_excel(writer,sheet_name='Общий свод',index=False)
+    df_freq_stats.to_excel(writer,sheet_name='Свод Несколько заявлений',index=False)
+    poo_svod_uniq_snils_df.to_excel(writer,sheet_name='Свод Уникальные СНИЛС',index=True)
+    poo_svod_dupl_snils_df.to_excel(writer,sheet_name='Свод Повторяющиеся СНИЛС',index=True)
+
+    poo_svod_all_df.to_excel(writer,sheet_name='Свод Все СНИЛС',index=True)
+    poo_svod_correct_snils_df.to_excel(writer,sheet_name='Свод Корректные СНИЛС',index=True)
+    poo_svod_non_correct_snils_df.to_excel(writer,sheet_name='Свод Некорректные СНИЛС',index=True)
+
+    # df.to_excel(writer,sheet_name='Общий список',index=False)
 
 
 print('Lindy Booth')
